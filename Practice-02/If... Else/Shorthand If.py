fa37d90c6690ead7c4933f82b05d83559d2e2296
Sp1r1t1337 <isauteshev2007@gmail.com>
2026-02-21 15:32:34 +0500

    Create Shorthand If.py

---
+++ Practice-02/If... Else/Shorthand If.py
@@ -0,0 +1,15 @@
+a = 10
+b = 5
+if a > b: print("a is bigger")
+
+print("Yes") if a > b else print("No")
+
+status = 1
+print("Active") if status == 1 else print("Inactive")
+
+x = 100
+y = 200
+if x < y: print("x is smaller")
+
+result = "Pass" if x > 50 else "Fail"
+print(result)
