1084f14b13c2c8c8754b6719ba205692392f199e
Sp1r1t1337 <isauteshev2007@gmail.com>
2026-02-21 15:32:11 +0500

    Create Python If Elif.py

---
+++ Practice-02/If... Else/Python If Elif.py
@@ -0,0 +1,30 @@
+temperature = 25
+
+if temperature > 30:
+    print("It is hot")
+elif temperature > 20:
+    print("It is warm")
+
+score = 85
+if score > 90:
+    print("Grade A")
+elif score > 80:
+    print("Grade B")
+
+hour = 14
+if hour < 12:
+    print("Morning")
+elif hour < 17:
+    print("Afternoon")
+
+balance = 0
+if balance > 0:
+    print("Positive")
+elif balance == 0:
+    print("Zero")
+
+speed = 60
+if speed > 100:
+    print("Fast")
+elif speed > 50:
+    print("Medium")
