a0735b60802818d3ff1b457b8196fe8880ec4817
Sp1r1t1337 <isauteshev2007@gmail.com>
2026-02-21 15:19:04 +0500

    Create Change List Items.py

---
+++ Practice-02/Lists/Change List Items.py
@@ -0,0 +1,19 @@
+tools = ["Hammer", "Saw", "Drill"]
+tools[1] = "Wrench"
+print(tools)
+
+colors = ["Red", "Blue", "Green", "Yellow"]
+colors[1:3] = ["Purple", "Orange"]
+print(colors)
+
+sizes = ["Small", "Large"]
+sizes[1:2] = ["Medium", "Extra Large"]
+print(sizes)
+
+letters = ["A", "B", "C"]
+letters.insert(1, "X")
+print(letters)
+
+items = ["Pen", "Pencil"]
+items[0] = "Ink"
+print(items)
