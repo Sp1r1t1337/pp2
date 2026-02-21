f87d1e221425fc86c98952bca32284e20874201c
Sp1r1t1337 <isauteshev2007@gmail.com>
2026-02-21 15:05:52 +0500

    Create Output Variables.py

---
+++ Practice-01/Variables/Output Variables.py
@@ -0,0 +1,17 @@
+message = "Python is powerful"
+print(message)
+
+part1 = "Hello "
+part2 = "World"
+print(part1 + part2)
+
+num1 = 50
+num2 = 10
+print(num1 + num2)
+
+label = "Score:"
+points = 100
+print(label, points)
+
+a, b, c = "one", "two", "three"
+print(a, b, c)
