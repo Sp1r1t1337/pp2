17cafd0ad271d09df3f20c1234a292cac3c1fc9c
Sp1r1t1337 <isauteshev2007@gmail.com>
2026-02-21 15:00:53 +0500

    Create statements.py

---
+++ Practice-01/Syntax/statements.py
@@ -0,0 +1,22 @@
+points = 150
+print(points)
+
+if points > 100:
+    print("High Score")
+else:
+    print("Keep Trying")
+
+calculation = 10 + \
+              20 + \
+              30
+print(calculation)
+
+colors = [
+    "Red",
+    "Green",
+    "Blue"
+]
+print(colors)
+
+length = 10; width = 5; area = length * width
+print(area)
