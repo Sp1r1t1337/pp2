58440189e5c572f6db0dafe61d286de054bd6844
Sp1r1t1337 <isauteshev2007@gmail.com>
2026-02-21 15:07:36 +0500

    Create Data Types.py

---
+++ Practice-01/Data Types/Data Types.py
@@ -0,0 +1,14 @@
+current_year = 2024
+print(type(current_year))
+
+product_price = 49.95
+print(type(product_price))
+
+user_id = "A123"
+print(type(user_id))
+
+is_available = True
+print(type(is_available))
+
+coordinates = ["12.5", "45.2", "10.0"]
+print(type(coordinates))
