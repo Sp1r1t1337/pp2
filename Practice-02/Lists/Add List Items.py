894b47a2943d4f154a259e6366b8b01b7e8f5455
Sp1r1t1337 <isauteshev2007@gmail.com>
2026-02-21 15:19:13 +0500

    Create Add List Items.py

---
+++ Practice-02/Lists/Add List Items.py
@@ -0,0 +1,20 @@
+planets = ["Mars", "Earth"]
+planets.append("Jupiter")
+print(planets)
+
+rivers = ["Nile", "Amazon"]
+rivers.insert(0, "Danube")
+print(rivers)
+
+phones = ["iPhone", "Samsung"]
+other_phones = ["Nokia", "Pixel"]
+phones.extend(other_phones)
+print(phones)
+
+parts = ["CPU", "GPU"]
+parts.extend(["RAM", "SSD"])
+print(parts)
+
+inventory = ["Apples"]
+inventory.append("Oranges")
+print(inventory)
