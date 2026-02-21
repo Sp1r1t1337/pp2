5b7b253422366d165b5056ddf5b49f6f7f25dfeb
Sp1r1t1337 <isauteshev2007@gmail.com>
2026-02-21 15:23:03 +0500

    Create Join Tuples.py

---
+++ Practice-02/Tuples/Join Tuples.py
@@ -0,0 +1,19 @@
+tuple1 = ("a", "b")
+tuple2 = (1, 2)
+tuple3 = tuple1 + tuple2
+print(tuple3)
+
+evens = (2, 4)
+doubled = evens * 2
+print(doubled)
+
+header = ("ID", "Name")
+row = (101, "Devin")
+data = header + row
+print(data)
+
+multi = ("Hi",) * 3
+print(multi)
+
+combined = (True,) + (False,)
+print(combined)
