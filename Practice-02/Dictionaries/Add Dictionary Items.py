e89099a812e05104cc54ad121233a45d151d892c
Sp1r1t1337 <isauteshev2007@gmail.com>
2026-02-21 15:28:38 +0500

    Create Add Dictionary Items.py

---
+++ Practice-02/Dictionaries/Add Dictionary Items.py
@@ -0,0 +1,17 @@
+player = {"name": "Jack"}
+player["health"] = 100
+print(player)
+
+player.update({"weapon": "Sword"})
+print(player)
+
+stock = {"apples": 50}
+stock["oranges"] = 30
+print(stock)
+
+stock.update({"pears": 20})
+print(stock)
+
+bag = {}
+bag["item1"] = "Book"
+print(bag)
