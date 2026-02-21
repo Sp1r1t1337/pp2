82412735a4a42c403a4609c2523e79931bcdc7f7
Sp1r1t1337 <isauteshev2007@gmail.com>
2026-02-21 15:41:41 +0500

    Create basic_functions.py

---
+++ Practice-03/functions/basic_functions.py
@@ -0,0 +1,25 @@
+def my_function():
+    print("Hello from a function")
+
+my_function()
+
+def show_date():
+    print("Today is Saturday")
+
+show_date()
+
+def alert_system():
+    print("System Check: OK")
+
+alert_system()
+
+def startup_msg():
+    print("Initializing...")
+    print("Ready.")
+
+startup_msg()
+
+def empty_task():
+    pass
+
+empty_task()
