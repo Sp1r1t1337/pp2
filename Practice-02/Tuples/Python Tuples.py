85a041a48a7b83e4fcb073c41df239370029c079
Sp1r1t1337 <isauteshev2007@gmail.com>
2026-02-21 15:21:23 +0500

    Create Python Tuples.py

---
+++ Practice-02/Tuples/Python Tuples.py
@@ -0,0 +1,14 @@
+brands = ("Apple", "Microsoft", "Google")
+print(brands)
+
+mixed_tuple = (1, "Hello", True, 3.14)
+print(mixed_tuple)
+
+single_item = ("Only",)
+print(type(single_item))
+
+duplicate_tuple = (5, 5, 10, 15)
+print(len(duplicate_tuple))
+
+tuple_constructor = tuple(("red", "blue"))
+print(tuple_constructor)
