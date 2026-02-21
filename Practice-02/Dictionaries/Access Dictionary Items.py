9a528a0a46ddeaf4b6416076e0c57c125f52284e
Sp1r1t1337 <isauteshev2007@gmail.com>
2026-02-21 15:28:22 +0500

    Create Access Dictionary Items.py

---
+++ Practice-02/Dictionaries/Access Dictionary Items.py
@@ -0,0 +1,10 @@
+laptop = {"brand": "Dell", "ram": "16GB", "cpu": "i7"}
+print(laptop["brand"])
+
+print(laptop.get("ram"))
+
+print(laptop.keys())
+
+print(laptop.values())
+
+print(laptop.items())
