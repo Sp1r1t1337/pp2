0cb3ab101b5833de73213fd675e78c2013dccaeb
Sp1r1t1337 <isauteshev2007@gmail.com>
2026-02-21 15:22:52 +0500

    Create Loop Tuples.py

---
+++ Practice-02/Tuples/Loop Tuples.py
@@ -0,0 +1,18 @@
+languages = ("Python", "Java", "C++")
+for x in languages:
+    print(x)
+
+for i in range(len(languages)):
+    print(languages[i])
+
+k = 0
+while k < len(languages):
+    print(languages[k])
+    k += 1
+
+scores = (95, 88, 72)
+for s in scores:
+    print(s)
+
+for val in ("A", "B"):
+    print(val)
