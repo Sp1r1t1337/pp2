b08bea78dca8a8a393d4bf22971d72a5f01374a6
Sp1r1t1337 <isauteshev2007@gmail.com>
2026-02-21 15:08:15 +0500

    Create Numbers.py

---
+++ Practice-01/Numbers/Numbers.py
@@ -0,0 +1,16 @@
+distance = 500
+print(type(distance))
+
+temperature = -12.5
+print(type(temperature))
+
+complex_num = 5 + 2j
+print(type(complex_num))
+
+raw_val = 15
+float_val = float(raw_val)
+print(float_val)
+
+decimal_val = 9.99
+integer_val = int(decimal_val)
+print(integer_val)
