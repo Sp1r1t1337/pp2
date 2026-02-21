81ccbc1decbd871c060cd562a63103c771e40cfa
Sp1r1t1337 <isauteshev2007@gmail.com>
2026-02-21 15:03:29 +0500

    Create Comments.py

---
+++ Practice-01/Comments/Comments.py
@@ -0,0 +1,17 @@
+print("Hello")
+# This is a hidden note
+
+# print("This will not run")
+print("This will run")
+
+print("Welcome") # This is an end-of-line comment
+
+# This is a long note
+# that spans multiple lines
+print("Multiple lines")
+
+"""
+This is a multiline
+string used as a comment
+"""
+print("End of examples")
