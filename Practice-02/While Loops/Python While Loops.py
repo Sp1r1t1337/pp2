7e4a61b17978b8c7ae27d044a895e358e5e19d3b
Sp1r1t1337 <isauteshev2007@gmail.com>
2026-02-21 15:34:13 +0500

    Create Python While Loops.py

---
+++ Practice-02/While Loops/Python While Loops.py
@@ -0,0 +1,27 @@
+i = 1
+while i < 6:
+    print(i)
+    i += 1
+
+count = 10
+while count > 0:
+    print("Countdown:", count)
+    count -= 1
+
+x = 0
+while x < 10:
+    x += 2
+    print("Even number:", x)
+
+msg = "Looping"
+index = 0
+while index < len(msg):
+    print(msg[index])
+    index += 1
+
+total = 0
+n = 1
+while n <= 5:
+    total += n
+    n += 1
+print("Final total:", total)
