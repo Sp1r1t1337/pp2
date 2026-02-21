dd9686f90eaa53e01405063336764c34db959d38
Sp1r1t1337 <isauteshev2007@gmail.com>
2026-02-21 15:24:30 +0500

    Create Access Set Items.py

---
+++ Practice-02/Sets/Access Set Items.py
@@ -0,0 +1,13 @@
+tags = {"Python", "Coding", "Tutorial"}
+for x in tags:
+    print(x)
+
+print("Coding" in tags)
+
+print("Java" not in tags)
+
+ids = {101, 102, 103}
+for i in ids:
+    print(i)
+
+print(105 in ids)
