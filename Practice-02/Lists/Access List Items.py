7a52b6aac8fd8cebd68ad6f9acba2b4092275981
Sp1r1t1337 <isauteshev2007@gmail.com>
2026-02-21 15:18:48 +0500

    Create Access List Items.py

---
+++ Practice-02/Lists/Access List Items.py
@@ -0,0 +1,11 @@
+animals = ["Lion", "Tiger", "Bear", "Wolf"]
+print(animals[1])
+
+print(animals[-1])
+
+numbers = [1, 2, 3, 4, 5, 6]
+print(numbers[2:5])
+
+print(numbers[:3])
+
+print(numbers[4:])
