bcff69adb5fe17d21f25b17d91fa24b8d878d321
Sp1r1t1337 <isauteshev2007@gmail.com>
2026-02-21 15:11:05 +0500

    Create Modify Strings.py

---
+++ Practice-01/Strings/Modify Strings.py
@@ -0,0 +1,11 @@
+entry = "  Room Service  "
+print(entry.upper())
+
+print(entry.lower())
+
+print(entry.strip())
+
+print(entry.replace("Room", "Table"))
+
+csv_data = "apple,orange,grape"
+print(csv_data.split(","))
