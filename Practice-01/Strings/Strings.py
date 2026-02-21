2cf451261ea31c479a6141ecc9152c2b5563437a
Sp1r1t1337 <isauteshev2007@gmail.com>
2026-02-21 15:10:19 +0500

    Create Strings.py

---
+++ Practice-01/Strings/Strings.py
@@ -0,0 +1,16 @@
+phrase = "Hello Python"
+print(phrase)
+
+multiline = """This is a string
+that covers multiple
+lines of text."""
+print(multiline)
+
+char = phrase[1]
+print(char)
+
+for letter in "Banana":
+    print(letter)
+
+text = "Testing the length"
+print(len(text))
