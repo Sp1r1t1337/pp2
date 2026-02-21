a3accc890c0793f6d7053240bc05b93cbb4bf616
Sp1r1t1337 <isauteshev2007@gmail.com>
2026-02-21 15:22:01 +0500

    Create Access Tuples.py

---
+++ Practice-02/Tuples/Access Tuples.py
@@ -0,0 +1,11 @@
+cities = ("London", "Paris", "New York", "Tokyo")
+print(cities[2])
+
+print(cities[-1])
+
+numbers = (10, 20, 30, 40, 50, 60)
+print(numbers[1:4])
+
+print(numbers[:3])
+
+print(numbers[-3:-1])
