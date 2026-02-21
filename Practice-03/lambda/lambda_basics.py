677da287c87b4d3393f9a09bb938c388d17e70a7
Sp1r1t1337 <isauteshev2007@gmail.com>
2026-02-21 15:43:31 +0500

    Create lambda_basics.py

---
+++ Practice-03/lambda/lambda_basics.py
@@ -0,0 +1,14 @@
+add_ten = lambda a : a + 10
+print(add_ten(5))
+
+multiply = lambda a, b : a * b
+print(multiply(5, 6))
+
+sum_three = lambda a, b, c : a + b + c
+print(sum_three(5, 6, 2))
+
+power = lambda x, y : x ** y
+print(power(2, 3))
+
+check_even = lambda x : x % 2 == 0
+print(check_even(4))
