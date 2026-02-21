0b1b88d3844e03b4f0288a044038685925281dcf
Sp1r1t1337 <isauteshev2007@gmail.com>
2026-02-21 15:33:06 +0500

    Create If Pass.py

---
+++ Practice-02/If... Else/If Pass.py
@@ -0,0 +1,20 @@
+a = 33
+b = 200
+
+if b > a:
+    pass
+
+def future_function():
+    pass
+
+if a == b:
+    pass
+else:
+    print("Not equal")
+
+class EmptyClass:
+    pass
+
+for x in [0, 1, 2]:
+    pass
+print("Loop finished")
