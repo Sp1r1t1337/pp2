e20d00636186b6fcaed093d332113bbff8b51e92
Sp1r1t1337 <isauteshev2007@gmail.com>
2026-02-21 15:24:14 +0500

    Create Python Sets.py

---
+++ Practice-02/Sets/Python Sets.py
@@ -0,0 +1,13 @@
+browsers = {"Chrome", "Firefox", "Edge"}
+print(browsers)
+
+duplicated_data = {1, 2, 2, 3, 3, 3}
+print(duplicated_data)
+
+mixed_set = {"Alpha", 100, True, 5.5}
+print(mixed_set)
+
+print(len(browsers))
+
+set_from_list = set(["North", "South", "East"])
+print(set_from_list)
