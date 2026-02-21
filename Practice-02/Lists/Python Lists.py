040d0822c7373986c9edc4fecc53e05d0721d79d
Sp1r1t1337 <isauteshev2007@gmail.com>
2026-02-21 15:18:38 +0500

    Create Python Lists.py

---
+++ Practice-02/Lists/Python Lists.py
@@ -0,0 +1,12 @@
+cities = ["Tokyo", "Paris", "Berlin"]
+print(cities)
+
+mixed_list = ["Car", 100, True, 4.5]
+print(mixed_list)
+
+duplicate_list = [10, 10, 20, 30]
+print(duplicate_list)
+
+print(len(cities))
+
+print(type(mixed_list))
