9d2b966ab400ea9fe0f6072939fd935c58c5842e
Sp1r1t1337 <isauteshev2007@gmail.com>
2026-02-21 15:05:22 +0500

    Create Assign Multiple Values.py

---
+++ Practice-01/Variables/Assign Multiple Values.py
@@ -0,0 +1,15 @@
+red, green, blue = 10, 20, 30
+print(red, green, blue)
+
+width = height = depth = 50
+print(width, height, depth)
+
+fruits = ["kiwi", "melon", "cherry"]
+x, y, z = fruits
+print(x, y, z)
+
+a = b = c = "Complete"
+print(a, b, c)
+
+start, end = 0, 100
+print(start, end)
