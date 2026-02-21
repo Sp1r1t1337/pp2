966b6c5c207d5c678a6669b0531c25b1469aafd5
Sp1r1t1337 <isauteshev2007@gmail.com>
2026-02-21 15:12:10 +0500

    Create String Methods.py

---
+++ Practice-01/Strings/String Methods.py
@@ -0,0 +1,14 @@
+data = "python programming"
+print(data.capitalize())
+
+search_text = "find the needle in the haystack"
+print(search_text.count("the"))
+
+check_val = "12345"
+print(check_val.isdigit())
+
+title_text = "welcome to the jungle"
+print(title_text.title())
+
+ending_check = "report.pdf"
+print(ending_check.endswith(".pdf"))
