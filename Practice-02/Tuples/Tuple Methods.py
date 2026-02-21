b9f279802d6e53322cc0040b8056c7121f90a589
Sp1r1t1337 <isauteshev2007@gmail.com>
2026-02-21 15:23:14 +0500

    Create Tuple Methods.py

---
+++ Practice-02/Tuples/Tuple Methods.py
@@ -0,0 +1,14 @@
+letters = ("a", "b", "c", "a", "d", "a")
+print(letters.count("a"))
+
+numbers = (10, 20, 30, 40, 50)
+print(numbers.index(30))
+
+repeat_check = (1, 2, 1, 3, 1)
+print(repeat_check.count(1))
+
+search_index = ("Gold", "Silver", "Bronze")
+print(search_index.index("Silver"))
+
+empty_check = (100, 200)
+print(len(empty_check))
