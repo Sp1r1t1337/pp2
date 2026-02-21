fe1fd3f0e3b787ce2d13b4ea95128fd601a67967
Sp1r1t1337 <isauteshev2007@gmail.com>
2026-02-21 15:02:09 +0500

    Create text.py

---
+++ Practice-01/Output/text.py
@@ -0,0 +1,16 @@
+fruit = "Apple"
+print(fruit)
+
+first_name = "Jane"
+last_name = "Doe"
+print(first_name, last_name)
+
+val1 = 20
+val2 = 30
+print(val1 + val2)
+
+print("Status:", "Active", "Level:", 5)
+
+city = "Paris"
+country = "France"
+print(city + country)
