910591954eb38e67a001a80be0fbeb0802cd7097
Sp1r1t1337 <isauteshev2007@gmail.com>
2026-02-21 15:19:31 +0500

    Create Remove List Items.py

---
+++ Practice-02/Lists/Remove List Items.py
@@ -0,0 +1,15 @@
+cars = ["BMW", "Tesla", "Ford", "Audi"]
+cars.remove("Tesla")
+print(cars)
+
+cars.pop(1)
+print(cars)
+
+cars.pop()
+print(cars)
+
+del cars[0]
+print(cars)
+
+cars.clear()
+print(cars)
