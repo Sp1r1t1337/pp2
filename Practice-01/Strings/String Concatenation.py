3105a51960bf8bea9ecd37ff479073c14525c400
Sp1r1t1337 <isauteshev2007@gmail.com>
2026-02-21 15:11:26 +0500

    Create String Concatenation.py

---
+++ Practice-01/Strings/String Concatenation.py
@@ -0,0 +1,19 @@
+part_one = "Good"
+part_two = "Morning"
+combined = part_one + part_two
+print(combined)
+
+space_combined = part_one + " " + part_two
+print(space_combined)
+
+start = "Level"
+end = "1"
+print(start + end)
+
+prefix = "Pre"
+root = "fix"
+print(prefix + root)
+
+greeting = "Hi"
+target = "User"
+print(greeting + ", " + target)
