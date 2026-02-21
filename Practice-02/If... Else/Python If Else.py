7a93807803d27a5865bb4ad063272eb14010cd4f
Sp1r1t1337 <isauteshev2007@gmail.com>
2026-02-21 15:32:22 +0500

    Create Python If Else.py

---
+++ Practice-02/If... Else/Python If Else.py
@@ -0,0 +1,30 @@
+age = 16
+
+if age >= 18:
+    print("Can vote")
+else:
+    print("Too young")
+
+count = 7
+if count % 2 == 0:
+    print("Even")
+else:
+    print("Odd")
+
+username = "admin"
+if username == "admin":
+    print("Welcome boss")
+else:
+    print("Welcome user")
+
+price = 100
+if price < 50:
+    print("Cheap")
+else:
+    print("Expensive")
+
+is_ready = False
+if is_ready:
+    print("System Online")
+else:
+    print("System Offline")
