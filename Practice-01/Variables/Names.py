cf0dd81a6877227b96915952a43a8f6a6c7a4181
Sp1r1t1337 <isauteshev2007@gmail.com>
2026-02-21 15:05:04 +0500

    Create Names.py

---
+++ Practice-01/Variables/Names.py
@@ -0,0 +1,14 @@
+my_home_town = "Chicago"
+print(my_home_town)
+
+userAge = 30
+print(userAge)
+
+TotalScore = 100
+print(TotalScore)
+
+var2 = "Test"
+print(var2)
+
+_private_id = 99
+print(_private_id)
