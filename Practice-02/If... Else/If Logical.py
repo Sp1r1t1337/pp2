ecf61ff6eb859f20744d3335b4289e2ca68c8faa
Sp1r1t1337 <isauteshev2007@gmail.com>
2026-02-21 15:32:44 +0500

    Create If Logical.py

---
+++ Practice-02/If... Else/If Logical.py
@@ -0,0 +1,18 @@
+x = 25
+y = 50
+z = 75
+
+if y > x and z > y:
+    print("Both conditions are True")
+
+if x > 10 or y < 20:
+    print("At least one is True")
+
+if not x == y:
+    print("x is not equal to y")
+
+if x < y and not x > z:
+    print("Logic check passed")
+
+if y == 50 or z == 100:
+    print("Valid input")
