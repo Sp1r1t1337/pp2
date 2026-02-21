9d65de4bea92d9fb17fbae203bcc8e9581ce7a85
Sp1r1t1337 <isauteshev2007@gmail.com>
2026-02-21 15:15:50 +0500

    Create Membership Operators.py

---
+++ Practice-02/Operators/Membership Operators.py
@@ -0,0 +1,12 @@
+colors = ["red", "blue", "green"]
+print("red" in colors)
+
+print("yellow" not in colors)
+
+text = "Python Programming"
+print("Python" in text)
+
+numbers = (10, 20, 30)
+print(40 not in numbers)
+
+print("Java" in text)
