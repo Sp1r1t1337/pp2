4df18019a5cffc07f0377009150a72eaad9c8eb9
Sp1r1t1337 <isauteshev2007@gmail.com>
2026-02-21 15:25:28 +0500

    Create Loop Sets.py

---
+++ Practice-02/Sets/Loop Sets.py
@@ -0,0 +1,16 @@
+values = {10, 20, 30, 40}
+for v in values:
+    print(v)
+
+words = {"Sun", "Moon", "Stars"}
+for w in words:
+    print("Value:", w)
+
+status_codes = {200, 404, 500}
+for code in status_codes:
+    print(code)
+
+for x in {"A", "B", "C"}:
+    print(x)
+
+[print(item) for item in {"App", "Web"}]
