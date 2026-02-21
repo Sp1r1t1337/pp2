293400f1be2f3381c5b47fb22a9f95f94eb3f051
Sp1r1t1337 <isauteshev2007@gmail.com>
2026-02-21 15:28:13 +0500

    Create Python Dictionaries.py

---
+++ Practice-02/Dictionaries/Python Dictionaries.py
@@ -0,0 +1,12 @@
+car = {"brand": "Tesla", "model": "Model 3", "year": 2023}
+print(car)
+
+user = {"id": 44, "active": True, "tags": ["admin", "staff"]}
+print(user)
+
+print(len(car))
+
+print(type(user))
+
+empty_dict = dict(status="pending", code=200)
+print(empty_dict)
