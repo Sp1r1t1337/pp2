0ddc0473ff8f9487457aab81528b53965fa9054a
Sp1r1t1337 <isauteshev2007@gmail.com>
2026-02-28 14:43:39 +0500

    Create Date Formatting (strftime).py

---
+++ Practice-04/Python Dates and Time/Date Formatting (strftime).py
@@ -0,0 +1,13 @@
+import datetime
+
+now = datetime.datetime.now()
+
+print(now.strftime("%A"))
+
+print(now.strftime("%B"))
+
+print(now.strftime("%d-%m-%Y"))
+
+print(now.strftime("%H:%M:%S %p"))
+
+print(now.strftime("%j"))
