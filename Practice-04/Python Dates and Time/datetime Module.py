f2d6851b8b5f52457cf6bc773845ed417041cd23
Sp1r1t1337 <isauteshev2007@gmail.com>
2026-02-28 14:43:00 +0500

    Create datetime Module.py

---
+++ Practice-04/Python Dates and Time/datetime Module.py
@@ -0,0 +1,14 @@
+import datetime
+
+x = datetime.datetime.now()
+print(x)
+
+print(x.year)
+print(x.month)
+print(x.day)
+
+y = datetime.date.today()
+print(y)
+
+z = datetime.time(12, 30, 45)
+print(z)
