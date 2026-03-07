9705b3649536eba20141d397e8921b1e0182367c
Sp1r1t1337 <isauteshev2007@gmail.com>
2026-03-07 10:26:18 +0500

    Update 5.py

--- Practice-05/RegEx exercises/5.py
+++ Practice-05/RegEx exercises/5.py
@@ -1,5 +1,8 @@
+import re
 def match_a_to_b(text):
-    pattern = r'a.*b$'
-    if re.search(pattern, text):
-        return "Match found"
-    return "No match"
+    if re.search(r'a.*b$', text):
+        return "Found a match!"
+    return "No match found."
+
+txt = input(str("Enter string: "))
+print(match_a_to_b(txt))
