1adc490d6e83ee2d1ba52a246ec2ae9c230fe388
Sp1r1t1337 <isauteshev2007@gmail.com>
2026-03-07 10:25:47 +0500

    Update 2.py

--- Practice-05/RegEx exercises/2.py
+++ Practice-05/RegEx exercises/2.py
@@ -1,3 +1,6 @@
+import re
 def match_a_two_three_b(text):
-    pattern = r'ab{2,3}'
-    return re.findall(pattern, text)
+    return re.findall(r'ab{2,3}', text)
+
+txt = input(str("Enter string: "))
+print(match_a_two_three_b(txt))
